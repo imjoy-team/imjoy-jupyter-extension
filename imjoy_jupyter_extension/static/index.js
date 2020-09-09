0eefea9941bc8b9ab4d6d3f6267a30ed8ff09ca1
Wei Ouyang <oeway007@gmail.com>
2020-09-09 17:31:33 +0200

    upgrade imjoy core

--- imjoy_jupyter_extension/static/index.js
+++ imjoy_jupyter_extension/static/index.js
@@ -395,7 +395,7 @@ define([
           mounted() {
             window.dispatchEvent(new Event('resize'));
             imjoyLoder.loadImJoyCore({
-              version: '0.13.31'
+              version: '0.13.35'
             }).then(imjoyCore => {
               console.log(`ImJoy Core (v${imjoyCore.VERSION}) loaded.`)
               const imjoy = new imjoyCore.ImJoy({
